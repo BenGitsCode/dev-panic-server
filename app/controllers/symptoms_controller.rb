38af6de789f739c4519cdba5a7ad69b1d099c831
benjamski <benjadamski@gmail.com>
2016-06-06 11:21:08 -0400

    Generated and scaffolded Symptoms
    configured .env settings

---
+++ app/controllers/symptoms_controller.rb
@@ -0,0 +1,59 @@
+class SymptomsController < ApplicationController
+  before_action :set_symptom, only: [:show, :update, :destroy]
+
+  # GET /symptoms
+  # GET /symptoms.json
+  def index
+    @symptoms = Symptom.all
+
+    render json: @symptoms
+  end
+
+  # GET /symptoms/1
+  # GET /symptoms/1.json
+  def show
+    render json: @symptom
+  end
+
+  # POST /symptoms
+  # POST /symptoms.json
+  def create
+    @symptom = Symptom.new(symptom_params)
+
+    if @symptom.save
+      render json: @symptom, status: :created, location: @symptom
+    else
+      render json: @symptom.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /symptoms/1
+  # PATCH/PUT /symptoms/1.json
+  def update
+    @symptom = Symptom.find(params[:id])
+
+    if @symptom.update(symptom_params)
+      head :no_content
+    else
+      render json: @symptom.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /symptoms/1
+  # DELETE /symptoms/1.json
+  def destroy
+    @symptom.destroy
+
+    head :no_content
+  end
+
+  private
+
+    def set_symptom
+      @symptom = Symptom.find(params[:id])
+    end
+
+    def symptom_params
+      params.require(:symptom).permit(:title, :solution, :url, :media, :private)
+    end
+end
---
+++ db/migrate/20160606151305_create_symptoms.rb
@@ -0,0 +1,13 @@
+class CreateSymptoms < ActiveRecord::Migration
+  def change
+    create_table :symptoms do |t|
+      t.string :title
+      t.text :solution
+      t.string :url
+      t.string :media
+      t.boolean :private
+
+      t.timestamps null: false
+    end
+  end
+end
