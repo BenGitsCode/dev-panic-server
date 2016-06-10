4d8f7188fa5f9268a105170f0ad68e2250cf32ca
benjamski <benjadamski@gmail.com>
2016-06-10 07:43:15 -0400

    Tested and failed
    getting same error still

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match '*path', via: [:options], to:  lambda {|_| [204, {'Content-Type' => 'text/plain'}, []]}
   resources :symptoms, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
